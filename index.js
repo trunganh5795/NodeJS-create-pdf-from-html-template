111d888e7971e888eb62a1a2c96684bb7537d5f1
truong ngoc trung anh <trunganh5795@github.com>
2023-11-17 16:56:53 +0700

    done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const pdf = require('pdf-creator-node');
 const fs = require('fs');
 const formService = require('./services/form');
+///////////////////
 const request_1 = {
   body: {
     formType: 'form1',
@@ -16,107 +17,13 @@ const request_1 = {
       'thanhTien',
       'ghiChu',
     ],
-    calulateSummary: true,
+    calulateSummary: {
+      key: 'thanhTien',
+    },
     records: [
       {
         maVatTu: 1,
-        tenVatTu: 2,
-        quyCach: 3,
-        nhanHieu: 4,
-        nuocSanXuat: 54,
-        donViTinh: 1,
-        soLuong: 9,
-        donGia: 190,
-        thanhTien: 9,
-        ghiChu: 99,
-      },
-      {
-        maVatTu: 1,
-        tenVatTu: 2,
-        quyCach: 3,
-        nhanHieu: 4,
-        nuocSanXuat: 54,
-        donViTinh: 1,
-        soLuong: 9,
-        donGia: 190,
-        thanhTien: 9,
-        ghiChu: 99,
-      },
-      {
-        maVatTu: 1,
-        tenVatTu: 2,
-        quyCach: 3,
-        nhanHieu: 4,
-        nuocSanXuat: 54,
-        donViTinh: 1,
-        soLuong: 9,
-        donGia: 190,
-        thanhTien: 9,
-        ghiChu: 99,
-      },
-      {
-        maVatTu: 1,
-        tenVatTu: 2,
-        quyCach: 3,
-        nhanHieu: 4,
-        nuocSanXuat: 54,
-        donViTinh: 1,
-        soLuong: 9,
-        donGia: 190,
-        thanhTien: 9,
-        ghiChu: 99,
-      },
-      {
-        maVatTu: 1,
-        tenVatTu: 2,
-        quyCach: 3,
-        nhanHieu: 4,
-        nuocSanXuat: 54,
-        donViTinh: 1,
-        soLuong: 9,
-        donGia: 190,
-        thanhTien: 9,
-        ghiChu: 99,
-      },
-      {
-        maVatTu: 1,
-        tenVatTu: 2,
-        quyCach: 3,
-        nhanHieu: 4,
-        nuocSanXuat: 54,
-        donViTinh: 1,
-        soLuong: 9,
-        donGia: 190,
-        thanhTien: 9,
-        ghiChu: 99,
-      },
-      {
-        maVatTu: 1,
-        tenVatTu: 2,
-        quyCach: 3,
-        nhanHieu: 4,
-        nuocSanXuat: 54,
-        donViTinh: 1,
-        soLuong: 9,
-        donGia: 190,
-        thanhTien: 9,
-        ghiChu: 99,
-      },
-      {
-        maVatTu: 1,
-        tenVatTu: 2,
-        quyCach: 3,
-        nhanHieu: 4,
-        nuocSanXuat: 54,
-        donViTinh: 1,
-        soLuong: 9,
-        donGia: 190,
-        thanhTien: 9,
-        ghiChu: 99,
-      },
-      {
-        maVatTu: 1,
-        tenVatTu: 2,
+        tenVatTu: '2 asA SAsSAw`1 AS A0d-2e asid- -12i asi -i-l',
         quyCach: 3,
         nhanHieu: 4,
         nuocSanXuat: 54,
@@ -190,7 +97,9 @@ const request_1 = {
 const request_2 = {
   body: {
     formType: 'form2',
-    calulateSummary: true,
+    calulateSummary: {
+      key: 'thanhTien',
+    },
     itemKeys: [
       'maVatTu',
       'tenVatTu',
@@ -206,7 +115,117 @@ const request_2 = {
     records: [
       {
         maVatTu: 1,
-        tenVatTu: 2,
+        tenVatTu:
+          'Kiểm tra sản phẩm abc,xyz, rt as,d asd,qwe ,qwe, qweqw,e ,qwe, q,we,,213,,1312 21312as asd 12 sad da',
+        quyCach: 3,
+        nhanHieu: 4,
+        nuocSanXuat: 54,
+        donViTinh: 1,
+        soLuong: 9,
+        donGia: 190,
+        thanhTien: 9,
+        ghiChu: 99,
+      },
+      {
+        maVatTu: 1,
+        tenVatTu:
+          'Kiểm tra sản phẩm abc,xyz, rt as,d asd,qwe ,qwe, qweqw,e ,qwe, q,we,,213,,1312 21312as asd 12 sad da',
+        quyCach: 3,
+        nhanHieu: 4,
+        nuocSanXuat: 54,
+        donViTinh: 1,
+        soLuong: 9,
+        donGia: 190,
+        thanhTien: 9,
+        ghiChu: 99,
+      },
+      {
+        maVatTu: 1,
+        tenVatTu: '1',
+        quyCach: 3,
+        nhanHieu: 4,
+        nuocSanXuat: 54,
+        donViTinh: 1,
+        soLuong: 9,
+        donGia: 190,
+        thanhTien: 9,
+        ghiChu: 99,
+      },
+      {
+        maVatTu: 1,
+        tenVatTu: '1',
+        quyCach: 3,
+        nhanHieu: 4,
+        nuocSanXuat: 54,
+        donViTinh: 1,
+        soLuong: 9,
+        donGia: 190,
+        thanhTien: 9,
+        ghiChu: 99,
+      },
+      {
+        maVatTu: 1,
+        tenVatTu: '1',
+        quyCach: 3,
+        nhanHieu: 4,
+        nuocSanXuat: 54,
+        donViTinh: 1,
+        soLuong: 9,
+        donGia: 190,
+        thanhTien: 9,
+        ghiChu: 99,
+      },
+      {
+        maVatTu: 1,
+        tenVatTu: '1',
+        quyCach: 3,
+        nhanHieu: 4,
+        nuocSanXuat: 54,
+        donViTinh: 1,
+        soLuong: 9,
+        donGia: 190,
+        thanhTien: 9,
+        ghiChu: 99,
+      },
+      {
+        maVatTu: 1,
+        tenVatTu: '1',
+        quyCach: 3,
+        nhanHieu: 4,
+        nuocSanXuat: 54,
+        donViTinh: 1,
+        soLuong: 9,
+        donGia: 190,
+        thanhTien: 9,
+        ghiChu: 99,
+      },
+      {
+        maVatTu: 1,
+        tenVatTu: '1',
+        quyCach: 3,
+        nhanHieu: 4,
+        nuocSanXuat: 54,
+        donViTinh: 1,
+        soLuong: 9,
+        donGia: 190,
+        thanhTien: 9,
+        ghiChu: 99,
+      },
+      {
+        maVatTu: 1,
+        tenVatTu: '1',
+        quyCach: 3,
+        nhanHieu: 4,
+        nuocSanXuat: 54,
+        donViTinh: 1,
+        soLuong: 9,
+        donGia: 190,
+        thanhTien: 9,
+        ghiChu: 99,
+      },
+      {
+        maVatTu: 1,
+        tenVatTu: 'sa tru oas qwp21 123las opas 213 asd1',
         quyCach: 3,
         nhanHieu: 4,
         nuocSanXuat: 54,
@@ -230,7 +249,6 @@ const request_2 = {
     },
   },
 };
-
 const request_3 = {
   body: {
     formType: 'form3',
@@ -246,7 +264,8 @@ const request_3 = {
     ],
     records: [
       {
-        noiDungKiemTra: 'Kiểm tra sản phẩm abc,xyz',
+        noiDungKiemTra:
+          'Kiểm tra sản phẩm abc,xyz, rt as,d asd,qwe ,qwe, qweqw,e ,qwe, q,we,,213,,1312 21312as asd 12 sad da',
         phuongPhapDo: 'Thủ công',
         thongSoThucTe: '300cm',
         thongSoThietKe: 300,
@@ -255,7 +274,8 @@ const request_3 = {
         ghiChu: 'Đạt rồi',
       },
       {
-        noiDungKiemTra: 'Kiểm tra sản phẩm abc,xyz',
+        noiDungKiemTra:
+          'Kiểm tra sản phẩm abc,xyz, 1, 2,1 ,tru,as ,qwe,hyu ,,32 ,32,1 112 31',
         phuongPhapDo: 'Thủ công',
         thongSoThucTe: '300cm',
         thongSoThietKe: 300,
@@ -601,16 +621,196 @@ const request_3 = {
     },
   },
 };
+const request_4 = {
+  body: {
+    formType: 'form4',
+    calulateSummary: false,
+    records: {
+      KCSCauHinh: {
+        title: 'KCS Cấu Hình',
+        content: [
+          'Số lượng rọ đựng vật hấp theo cấu hình chào',
+          'Dây cáp nguồn: 01 cái, kích thước 2x4mm2',
+          'Dây nối đất: 01 cái, dài 2m, kích thước 1x1mm2',
+          'Sách hướng dẫn sử dụng: 01 quyển',
+          'Quy trình vận hành: 01 tờ (dán trên máy)',
+          'Đồng hồ đo và theo dõi áp suất buồng khử khuẩn (mặt trước): -1 kgf/cm2 đến 5 kgf/cm2',
+          'Aptomat tổng: 1P, 32A, chức năng chống dòng dò',
+          'Công suất thanh đốt (50,75 lít : 4kW / 100 lít: 4,5kW)',
+          'Đầy đủ tem nhãn cho công tắc, đèn báo, mác sản phẩm',
+        ],
+      },
+      KCSLapRap: {
+        title: 'KCS Lắp ráp',
+        content: [
+          'Tất cả các vị trí đấu dây, hàn dây chắc chắn, không bị tuột khi chịu lựu kéo bằng tay',
+          'Các vị trí đi dây đúng theo quy định, các vị trí buộc dây khi lắp ráp đúng vị trí, buộc dây chắc chắn',
+          'Kích thước, màu dây và đánh số dây đúng như bản vẽ',
+          'Mở và khóa nắp buồng hấp dễ dàng, và chắc chắn',
+          'Thanh đốt không bị cong, vênh, trồi lên khỏi mực nước trong nồi (khi đủ nước)',
+          'Đo điện trở thanh đốt ~ 12 ôm (R=U2/P)',
+          'Đo trở kháng cách điện giữa thanh đốt với vỏ máy',
+        ],
+      },
+      KCSChucNangHoatDong: {
+        title: 'KCS Chức Năng Hoạt Động',
+        content: [
+          'Màn hình LED: Sáng rõ, không bị nhiễu',
+          'Test chức năng, độ nhạy của các phím điều khiển',
+          'Kiểm tra tham số cài đặt 10 chương trình mặc định đúng như cấu hình kỹ thuật',
+          'Test chức năng chọn chương trình (cài đặt sẵn và tùy chọn), cài đặt tham số, hiệu chỉnh tham số chương trình',
+          'Không dò rỉ hơi hoặc nước trong quá trình chạy thử',
+          'Van an toàn tự động mở khi áp suất ≥ 2,7kgf/cm2 và tự động đóng khi áp suất ≤ 2.5 kgf/cm2',
+          'Các đèn báo heat, power, water sáng rõ và hoạt động đúng chức năng',
+          'Test chức năng hoạt động và hiển thị nút thoát hiểm Exhaust',
+          'Test lỗi E1: Khóa van cấp nước sau đó chạy nồi hấp, kiểm tra xem thiết bị có báo lỗi E1 hay không (sau 15p)',
+          'Test lỗi E2: Khóa van cấp nước trong quá trình tiệt trùng, kiểm tra xem thiết bị có báo lỗi E2 hay không',
+          'Test lỗi E3: Ngắt tín hiệu cảm biến nhiệt độ, kiểm tra xem thiết bị có báo lỗi E3 hay không',
+          'Test lỗi E4: Rút role điều khiển của thanh đốt. Kiểm tra xem nồi hấp có báo lỗi E4 hay không',
+          'Chức năng hoạt động đóng mở của các van điện bình thường, không có tiếng kêu lạch cạch khi van mở',
+          'Chức năng báo cạn nước',
+          'Chạy thử ít nhất 3 chương trình hoàn chỉnh Chường trình 3; Chường trình 6; Chường trình 10',
+          'Các đường ống kín không bị rò rỉ',
+          'Gioăng thiết bị không bị rò rỉ',
+          'Cơ cấu đóng, mở nắp nồi nhẹ nhàng',
+        ],
+      },
+    },
+    thongTinHeader: {
+      maLenh: 12,
+      nguoiLap: 'user 123456',
+      ngayLap: '20/10/2023',
+      maSanPham: 'ASPPO',
+      boPhan: 'BP1',
+      maSo: 99900,
+      phienBan: 5.0,
+      NgayBanHanh: '20/09/2023',
+      congDoan: 1,
+      soSeri: 99099,
+    },
+  },
+};
+/////////////////
+const kiemTraCongDoanCol = [
+  {
+    key: 'noiDungKiemTra',
+    title: 'Nội dung kiểm tra',
+    width: 200,
+  },
+  {
+    key: 'phuongPhapDo',
+    title: 'Phương pháp đo',
+    width: 80,
+  },
+  {
+    key: 'thongSoThucTe',
+    title: 'Thông số thực tế',
+    width: 80,
+  },
+  {
+    key: 'thongSoThietKe',
+    title: 'Thông số thiết kết',
+    width: 80,
+  },
+  {
+    key: 'soLuongDat',
+    title: 'Số lượng đạt',
+    width: 80,
+  },
+  {
+    key: 'soLuongKhongDat',
+    title: 'Số lượng không đạt',
+    width: 80,
+  },
+  {
+    key: 'ghiChu',
+    title: 'Ghi chú',
+    width: 130,
+  },
+];
+
+const deNghiMuaVatTuCol = [
+  {
+    key: 'maVatTu',
+    title: 'Mã vật tư',
+    width: 70,
+  },
+  {
+    key: 'tenVatTu',
+    title: 'Tên vật tư',
+    width: 270,
+  },
+  {
+    key: 'quyCach',
+    title: 'Quy cách',
+    width: 50,
+  },
+  {
+    key: 'nhanHieu',
+    title: 'Nhãn Hiệu',
+    width: 50,
+  },
+  {
+    key: 'nuocSanXuat',
+    title: 'Nước SX',
+    width: 50,
+  },
+  {
+    key: 'donViTinh',
+    title: 'Đơn vị tính',
+    width: 70,
+  },
+  {
+    key: 'soLuong',
+    title: 'Số lượng',
+    width: 50,
+  },
+  {
+    key: 'donGia',
+    title: 'Đơn giá',
+    width: 50,
+  },
+  {
+    key: 'thanhTien',
+    title: 'Thành tiền',
+    width: 50,
+  },
+  {
+    key: 'ghiChu',
+    title: 'Ghi Chú',
+    width: 100,
+  },
+];
+
+const phieuKCSCol = [
+  {
+    key: 'noiDungKiemTra',
+    title: 'Nội dung kiểm tra',
+    width: 300,
+  },
+  {
+    key: 'dat',
+    title: 'Đạt',
+    width: 50,
+  },
+  {
+    key: 'khongDat',
+    title: 'Không Đạt',
+    width: 70,
+  },
+  {
+    key: 'ghiChu',
+    title: 'Ghi chú',
+    width: 150,
+  },
+];
+/////////////////
 async function getPdfDonHang(request, response, conn) {
   try {
     console.log('running to here....');
-    let { records, thongTinHeader, formType, itemKeys } = request.body;
-    // donHangTemp = utilities.convertMapToJson(donHangTemp);
-    // let donHang = donHangTemp['data'];
-    // // console.log('donHang===', donHang)
-    // if (!donHang || JSON.stringify(donHang) === JSON.stringify({}))
-    //   return __dirname + '/html2pdf/output_template.pdf';
-    // Read HTML Template
+    let { records, thongTinHeader, formType, calulateSummary, col } =
+      request.body;
+
     var html = fs.readFileSync(
       __dirname + '/template/html/template.html',
       'utf8'
@@ -623,75 +823,54 @@ async function getPdfDonHang(request, response, conn) {
       __dirname + `/template/html/${formType}/header.html`,
       'utf8'
     );
-
-    var tableContent = fs.readFileSync(
-      __dirname + `/template/html/${formType}/content.html`,
-      'utf8'
-    );
-
     var cFooter = fs.readFileSync(
       __dirname + `/template/html/${formType}/footer.html`,
       'utf8'
     );
+
+    /////////////////////////////////////////////
     html = html.replace('<!-- CSS -->', cCSS);
-    // update header
     html = html.replace('<!-- Header -->', cHeader);
     html = html.replace('<!-- Footer -->', cFooter);
-    tableContent = tableContent.replace(
-      '<!-- content -->',
-      formService.generateContentForm1(records, itemKeys)
-    );
-    html = html.replace('<!-- Content -->', tableContent);
+    /////////////////////////////////////////////
+    // select generate conntent function
+    if (formType !== 'form4') {
+      html = html.replace(
+        '<!-- Content -->',
+        formService.generateContentTable(records, col, calulateSummary)
+      );
+    } else {
+      html = html.replace(
+        '<!-- Content -->',
+        formService.generateContentTableForm4(records, col, calulateSummary)
+      );
+    }
 
-    // comment here
-    // // update hang hoa
-    // let temp = capNhatHangHoa(cHangHoa, cTong, donHang);
-
-    // cTongHangHoa = cTongHangHoa.replace(
-    //   '<!-- DanhSachHangHoa -->',
-    //   temp.cHangHoa
-    // );
-    // cTongHangHoa = cTongHangHoa.replace('<!-- TongHangHoa -->', temp.cTong);
-    // html = html.replace('<!-- Tong_Hang_Hoa_DonHang -->', cTongHangHoa);
-
-    // // update boc tra
-    // let danhSachDiemBoc = capNhatDiemBocTra(
-    //   cDiemBocTra,
-    //   cHangHoaBocTra,
-    //   cMoRongDiemBocTra,
-    //   donHang,
-    //   true
-    // );
-    // let danhSachDiemTra = capNhatDiemBocTra(
-    //   cDiemBocTra,
-    //   cHangHoaBocTra,
-    //   cMoRongDiemBocTra,
-    //   donHang,
-    //   false
-    // );
-
-    // html = html.replace('<!-- Danh_Sach_DiemGiaoHang -->', danhSachDiemBoc);
-    // html = html.replace('<!-- Danh_Sach_DiemNhanHang -->', danhSachDiemTra);
-
-    // update gia cuoc
-    // cGiaCuoc = capNhatGiaCuoc(cGiaCuoc, donHang);
-    // html = html.replace('<!-- Gia_Cuoc -->', cGiaCuoc);
-    // let uuid = donHang['soPhieu'] + '_' + utilities.generateUUIDValue();
     fs.writeFileSync(
-      __dirname + '/template/output/' + Math.random() + '.html',
+      __dirname +
+        '/template/output/' +
+        new Date().getTime() +
+        '_' +
+        formType +
+        '.html',
       html
     );
-    //// comment here
-
+    // conver logo image to base64
     const bitmap = fs.readFileSync(
       __dirname + '/template/html/assets/imgs/logo.png'
     );
     const logo = bitmap.toString('base64');
+    ////
     var document = {
       html: html,
       data: {},
-      //   path: __dirname + '/html2pdf/output' + uuid + '.pdf',
-      path: __dirname + '/template/output/' + Math.random() + '.pdf',
+      path:
+        __dirname +
+        '/template/output/' +
+        new Date().getTime() +
+        '_' +
+        formType +
+        '.pdf',
       type: '',
       data: {
         thongTinHeader: {
@@ -701,19 +880,25 @@ async function getPdfDonHang(request, response, conn) {
 
         config: {
           __dirname: __dirname,
+          columns: col,
         },
       },
     };
-    // By default a file is created but you could switch between Buffer and Streams by using "buffer" or "stream" respectively.
-
+    /// CREATE PDF ////
     await pdf
       .create(document, {
-        format: 'A3',
-        orientation: 'portrait',
+        format: 'A4',
+        orientation: 'landscape',
         childProcessOptions: {
           env: {
             OPENSSL_CONF: '/dev/null',
           },
+        },
+        border: {
+          top: '2cm',
+          right: '1cm',
+          bottom: '2cm',
+          left: '1.5cm',
         },
       })
       .then((res) => {
@@ -731,7 +916,17 @@ async function getPdfDonHang(request, response, conn) {
 }
 
 //Form mua vật tư
-// getPdfDonHang(request_1);
+request_1.body.col = deNghiMuaVatTuCol;
+getPdfDonHang(request_1);
 
 //Form lĩnh vật tư
+request_2.body.col = deNghiMuaVatTuCol;
+getPdfDonHang(request_2);
+
+//Form kiểm tra công đoạn
+request_3.body.col = kiemTraCongDoanCol;
 getPdfDonHang(request_3);
+
+//Form KCS
+request_4.body.col = phieuKCSCol;
+getPdfDonHang(request_4);
--- services/form.js
+++ services/form.js
@@ -1,52 +1,72 @@
-const emptyLine = `<tr>
-        <td colspan="11" style="border: none">&nbsp;</td>
-      </tr>`;
-generateContentForm1 = (data, itemKeys, calulateSummary) => {
-  let numberOfLineOnPage = 22;
-  /* everypage contains 24 lines
-  first page includes : header : 6 lines, 1 empty lines
-  table: 1 line for thead, every record is 1 line
-  1 line for summary ( total : .....) 
-  signature section : 9 lines
-  */
-  let currentLine = 7;
-
+const fs = require('fs');
+generateContentTable = (data, itemKeys, calulateSummary) => {
   let contentHTLM = data.reduce((html, item, index) => {
-    currentLine++;
-    if (currentLine > numberOfLineOnPage) {
-      html += ` </tr>
-    <td colspan="11" style="border: none">&nbsp;</td>
-    </tr> 
-    `;
-      currentLine = 1;
+    html += `<table style="margin-top:-1px"><tr><td style="width:38px" class="text-center">
+    ${index + 1}
+    </td>`;
+    for (let i = 0; i < itemKeys.length; i++) {
+      html += `<td style="width:${itemKeys[i].width}px">
+      ${item[itemKeys[i].key]}
+      </td>`;
     }
-
-    html += `<tr><td>${index + 1}</td>`;
-    for (let key of itemKeys) {
-      html += `<td>${item[key]}</td>`;
-    }
-    html += `</tr>`;
+    html += `</tr></table>`;
 
     return html;
   }, '');
   if (calulateSummary) {
-    if (currentLine > numberOfLineOnPage) {
-      contentHTLM += ` </tr>
-    <td colspan="11" style="border: none">&nbsp;</td>
-    </tr> 
-    `;
-      currentLine = 0;
+    const total = data.reduce(
+      (total, item, index) => {
+        total.value += item[calulateSummary.key];
+        return total;
+      },
+      { totalColWidth: 0, valueColWidth: 0, value: 0 }
+    );
+    for (let i = 0; i < itemKeys.length; i++) {
+      if (i <= itemKeys.length - 3) {
+        total.totalColWidth += itemKeys[i].width;
+      } else {
+        total.valueColWidth += itemKeys[i].width;
+      }
     }
-    currentLine++;
-    contentHTLM += `<tr>
-      <td colspan="9" class="text-right">Tổng tiền:</td>
-      <td colspan="2" >1</td>
-    </tr>`;
-  }
-
-  if (currentLine >= numberOfLineOnPage - 8) {
-    contentHTLM += emptyLine.repeat(numberOfLineOnPage - currentLine + 1);
+    contentHTLM += `<table style="margin-top:-1px">
+    <tr>
+    <td class="text-center" style="width:${total.totalColWidth + 38}px">Tổng
+    </td>
+       <td class="text-center" style="width:${total.valueColWidth}px">
+       ${total.value}
+    </td>
+    </tr>
+    </table>
+    `;
   }
   return contentHTLM;
 };
-module.exports = { generateContentForm1 };
+
+generateContentTableForm4 = (data, itemKeys) => {
+  console.log(Object.keys(data));
+  const contentHTML = Object.keys(data).reduce((html, key, index) => {
+    html += `<table style="margin-top:-1px">
+      <tr>
+      <th>${data[key].title}</th>
+      </tr>
+      </table>
+      `;
+    html += data[key].content.reduce((row, item, index) => {
+      row += `<table style="margin-top:-1px">
+      <tr>
+      <td style="width:38px">${index + 1}</td>
+      <td style="width:${itemKeys[0].width}px">${item}</td>
+      <td style="width:${itemKeys[1].width}px">&nbsp;</td>
+      <td style="width:${itemKeys[2].width}px">&nbsp;</td>
+      <td style="width:${itemKeys[3].width}px">&nbsp;</td>
+      </tr>
+      </table>
+      `;
+      return row;
+    }, '');
+    return html;
+  }, '');
+  console.log(contentHTML);
+  return contentHTML;
+};
+module.exports = { generateContentTable, generateContentTableForm4 };
